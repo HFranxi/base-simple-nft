5ef5a82551e2bd0679d20141918ccc818fdd4412
HFranxi <82986762+HFranxi@users.noreply.github.com>
2026-03-11 09:11:19 +0700

    add nft contract

--- contracts/BaseNFT.sol
+++ contracts/BaseNFT.sol
@@ -1,1 +1,16 @@
+pragma solidity ^0.8.20;
 
+contract BaseNFT {
+
+    uint256 public tokenId;
+
+    mapping(uint256 => address) public owner;
+
+    function mint() public {
+
+        tokenId++;
+
+        owner[tokenId] = msg.sender;
+
+    }
+}
