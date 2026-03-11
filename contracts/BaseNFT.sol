84c8b8da45cc3c3bdc2bbeb8413d5bfd41c47d37
HFranxi <82986762+HFranxi@users.noreply.github.com>
2026-03-11 11:40:36 +0700

    Update BaseNFT.sol

--- contracts/BaseNFT.sol
+++ contracts/BaseNFT.sol
@@ -1,16 +1,13 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 contract BaseNFT {
-
     uint256 public tokenId;
 
     mapping(uint256 => address) public owner;
 
     function mint() public {
-
         tokenId++;
-
         owner[tokenId] = msg.sender;
-
     }
 }
